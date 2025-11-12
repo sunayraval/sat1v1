6aea9e9a642cb61839b199327e5331f5b30f8262
Sunay <sunay.raval12@gmail.com>
2025-11-12 18:20:55 -0500

    styles 4.0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,6 +9,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
+// Global theme: import after index.css so variables and utility layers are available.
+import "./styles/neon.css";
 
 createRoot(document.getElementById("root")!).render(
 	<ErrorBoundary>
